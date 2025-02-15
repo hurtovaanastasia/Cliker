f5fcc159dd8fa17ef4864e7495ae768d1c28b662
anastasia <anastasia@hurto>
2025-02-15 10:38:57 +0300

    Изменила расцветку и исправила ошибку поворота экрана

--- app/src/main/java/com/example/cliker/MainActivity.java
+++ app/src/main/java/com/example/cliker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.cliker;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private Integer count = 0;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,71 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textView = findViewById(R.id.txtCounter);
+        textView.setText(count.toString());
+        if (savedInstanceState != null && savedInstanceState.containsKey("Klick")) {
+            count = savedInstanceState.getInt("Klick");
+            textView.setText(count.toString());
+        }
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("Instance", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("Instance", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    //изм
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("Instance", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("Instance", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("Instance", "onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Klick", count);
+        Log.d("Instance", "onSave");
+        Toast.makeText(this, "onSave", Toast.LENGTH_LONG).show();
+
+    }
+    protected void onRestoreInstanceState(Bundle SavedState){
+        super.onRestoreInstanceState(SavedState);
+        if(SavedState != null && SavedState.containsKey("Klick")){
+            count = SavedState.getInt("Klick");
+        }
+        Log.d("Instens","get");
+        Toast.makeText(this, "get",Toast.LENGTH_LONG).show();
     }
     public void onClickBatton(View view) {
         count++;
